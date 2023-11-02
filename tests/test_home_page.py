6ca68ec4797b070c5a82b1181d6ec49e6101b4c9
AlexeyZh <waltafunk@gmail.com>
2023-11-02 21:34:37 +0300

    Updated home page title tests
    Parametrized tests for home page title have been updated to include both positive and negative scenarios.

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,8 +1,19 @@
+import pytest
+
 from pages.home_page import HomePage
 
 
 # Проверка заголовка страницы
-def test_home_page_title(page):
+@pytest.mark.smoke
+@pytest.mark.parametrize('url, expected_title, test_type', [
+    ('https://www.demoblaze.com/', 'STORE', 'positive'),
+    ('https://www.demoblaze.com/', 'Wrong Title', 'negative'),
+])
+def test_home_page_title(page, url, expected_title, test_type):
     home_page = HomePage(page)
     home_page.go_to()
-    assert home_page.get_title() == "STORE"
+    if test_type == 'positive':
+        assert home_page.get_title() == expected_title, f"Wrong title for {url}"
+    elif test_type == 'negative':
+        assert home_page.get_title() != expected_title, f"Title should not be {expected_title}"
+
