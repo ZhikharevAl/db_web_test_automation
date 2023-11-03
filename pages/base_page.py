eb2484714ee1df20213d6b2d4e40cfa52bf2618a
AlexeyZh <waltafunk@gmail.com>
2023-11-03 22:07:49 +0300

    feat: add URL and go_to, click_button methods to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,5 +2,13 @@ from playwright.sync_api import Page
 
 
 class BasePage:
+    URL = "https://www.demoblaze.com/"
+
     def __init__(self, page: Page):
         self.page = page
+
+    def go_to(self):
+        self.page.goto(self.URL)
+
+    def click_button(self, selector: str):
+        self.page.click(selector)
