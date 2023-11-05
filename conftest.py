46a7887a9a67df328c5799b71626465c3ac450fb
AlexeyZh <waltafunk@gmail.com>
2023-11-05 14:44:40 +0300

    feat: add url fixture

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch()  # Запуск браузера
+        browser = p.chromium.launch(headless=False)  # Запуск браузера
         yield browser
         browser.close()  # Закрытие браузера после теста
 
@@ -15,3 +15,11 @@ def page(browser):
     page = browser.new_page()  # Открытие новой страницы в браузере
     yield page
     page.close()  # Закрытие страницы после теста
+
+
+@pytest.fixture(params=[
+    ('https://www.demoblaze.com/', 'STORE', 'positive'),
+    ('https://www.demoblaze.com/', 'Wrong Title', 'negative'),
+])
+def url(request):
+    return request.param
