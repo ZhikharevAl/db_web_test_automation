abcaed227277f6f1821268555e1d747477f19192
AlexeyZh <waltafunk@gmail.com>
2023-11-15 02:54:20 +0300

    test: refactor

--- tests/test_register_and_login.py
+++ tests/test_register_and_login.py
@@ -5,7 +5,6 @@ import pytest
 from allure_commons.types import Severity
 
 from pages.resgister_and_login_page import RegisterAndLoginPage
-from pages.locators import LoginPageLocators
 
 
 @pytest.mark.smoke
@@ -13,14 +12,13 @@ from pages.locators import LoginPageLocators
 def test_register_and_login_functionality(page, caplog):
     caplog.set_level(logging.INFO)
     register_and_login_page = RegisterAndLoginPage(page)
-    selector = LoginPageLocators.USERNAME_DISPLAY_SELECTOR
+
     with allure.step("Открываем страницу регистрации"):
         register_and_login_page.go_to()
     with allure.step("Регистрируем нового пользователя"):
         username = 'username'
         password = 'password'
         register_and_login_page.register_and_login(username, password)
-    page.wait_for_selector(selector)
     with (allure.step("Проверяем правильность регистрации")):
         assert register_and_login_page.is_logged_in(username) is True, \
             f"Неправильный результат для {username} и {password}"
