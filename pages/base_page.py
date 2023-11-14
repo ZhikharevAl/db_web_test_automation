6d1092be12baf5523250c4e90dedd198c904b4c4
AlexeyZh <waltafunk@gmail.com>
2023-11-15 02:52:25 +0300

    feat: add wait_for_selector

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,6 @@ from playwright.sync_api import Page
 
 
 class BasePage:
-    # URL = "https://www.demoblaze.com/"
     URL = "https://www.demoblaze.com/"
 
     def __init__(self, page: Page):
@@ -10,6 +9,9 @@ class BasePage:
 
     def go_to(self):
         self.page.goto(self.URL)
+
+    def wait_for_selector(self, selector: str):
+        self.page.wait_for_selector(selector)
 
     def click_button(self, selector: str):
         self.page.click(selector)
