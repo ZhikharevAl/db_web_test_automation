b37f6daca2dd9cce643e729b6100ff22186eda62
AlexeyZh <waltafunk@gmail.com>
2023-11-14 03:25:05 +0300

    feat: add CLOSE_BUTTON locator

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,5 @@ class SignUpPageLocators:
     USERNAME_INPUT = "#sign-username"
     PASSWORD_INPUT = "#sign-password"
     SIGNUP_SUBMIT_BUTTON = "button[onclick='register()']"
+    CLOSE_BUTTON = "#signInModal > div > div > div.modal-footer > button.btn.btn-secondary"
+
