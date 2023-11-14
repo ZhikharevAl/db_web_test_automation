41b9fc50ee3c2856bb7e03fbdfdf3246316703cc
AlexeyZh <waltafunk@gmail.com>
2023-11-14 03:27:27 +0300

    test: fix login_functionality depending on new user registration

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 import allure
 import logging
@@ -5,11 +7,13 @@ import logging
 from allure_commons.types import Severity
 
 from pages.login_page import LoginPage
+from tests.test_signup import test_signup_functionality
 
 
 @pytest.mark.smoke
 @allure.severity(Severity.CRITICAL)
 @pytest.mark.parametrize('username, password, test_type', [
+    ('valid_username', 'valid_password', 'positive'),
     ('invalid_username', 'valid_password', 'negative'),
     ('valid_username', 'invalid_password', 'negative'),
     ('invalid_username', 'invalid_password', 'negative'),
@@ -17,17 +21,23 @@ from pages.login_page import LoginPage
 def test_login_functionality(page, username, password, test_type, caplog):
     caplog.set_level(logging.INFO)
     login_page = LoginPage(page)
+    username = 'username'
+    password = 'password'
+
     with allure.step("Открываем страницу входа"):
         login_page.go_to()
-    with allure.step("Вводим логин и пароль"):
-        login_page.login(username, password)
-    with ((allure.step("Проверяем правильность входа"))):
-        if test_type == 'positive':
-            assert login_page.is_logged_in(username) is True, \
-                f"Wrong result for {username} and {password}"
-            logging.info(f"Login is successful for {username}")
-        elif test_type == 'negative':
-            assert login_page.is_logged_in(
-                username) is False, \
-                f"Login should not be successful for {username} and {password}"
-            logging.info(f"Login is not successful as expected for {username}")
+    with allure.step("Регистрируем нового пользователя"):
+        signup_successful = test_signup_functionality(page, username, password, test_type='positive', caplog=caplog)
+    if signup_successful:
+        with allure.step("Вводим логин и пароль"):
+            login_page.login(username, password)
+        with allure.step("Проверяем правильность входа"):
+            if test_type == 'positive':
+                assert login_page.is_logged_in(username) is True, \
+                    f"Wrong result for {username} and {password}"
+                logging.info(f"Login is successful for {username}")
+            elif test_type == 'negative':
+                assert login_page.is_logged_in(
+                    username) is False, \
+                    f"Login should not be successful for {username} and {password}"
+                logging.info(f"Login is not successful as expected for {username}")
