b87863530a25f8d285ec969662887cbe6ade3dde
AlexeyZh <waltafunk@gmail.com>
2023-11-14 03:28:15 +0300

    test: add test_login_functionality

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,9 +1,12 @@
+import time
+
 import allure
 import pytest
 from allure_commons.types import Severity
 
 from tests.test_home_page import test_home_page_title
 from tests.test_login import test_login_functionality
+from tests.test_signup import test_signup_functionality
 
 
 class TestE2E:
@@ -12,14 +15,14 @@ class TestE2E:
     def test_e2e(self, page, url, caplog):
         expected_title, test_type = url[1], url[2]
         url = url[0]
-        username = 'username'  # Замените на вашу фикстуру или значение
-        password = 'password'  # Замените на вашу фикстуру или значение
+        username = 'username'
+        password = 'password'
 
         with allure.step("Проверка заголовка домашней страницы"):
             test_home_page_title(page, url, expected_title, test_type, caplog)
+        with allure.step("Проверка правильности регистрации"):
+            test_signup_functionality(page, username, password, test_type, caplog)
         with allure.step("Проверка функциональности входа в систему"):
-            # TODO: Временно проверяем только на test_type = 'negative'
-            test_type = 'negative'
             test_login_functionality(page, username,
                                      password, test_type, caplog)
     # TODO: Добавить другие тесты, чтобы сделать его полноценным e2e тестом
