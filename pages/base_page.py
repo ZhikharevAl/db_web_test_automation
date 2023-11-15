f64aab671b45ee2b7afb7e73a8c779a0c474e99d
AlexeyZh <waltafunk@gmail.com>
2023-11-15 19:06:52 +0300

    feat: add title, inner_text, query_selector_all methods Descriptions:     - title: Returns the title     - inner_text: Accepts a selector and returns the text of the element selected by that selector.     - query_selector_all: Accepts a selector and returns all elements on the page that match that selector.

--- pages/base_page.py
+++ pages/base_page.py
@@ -23,6 +23,40 @@ class BasePage:
         """
         self.page.goto(self.URL)
 
+    def click_button(self, selector: str):
+        """
+        Кликает по кнопке, соответствующей указанному селектору.
+
+        :param selector: Селектор кнопки, по которой нужно кликнуть.
+        """
+        self.page.click(selector)
+
+    def fill(self, selector: str, value: str):
+        """
+        Заполняет поле ввода, выбранное селектором, указанным значением.
+
+        :param selector: Селектор поля ввода, которое нужно заполнить.
+        :param value: Значение, которым нужно заполнить поле ввода.
+        """
+        self.page.fill(selector, value)
+
+    def title(self):
+        """
+        Возвращает заголовок текущей страницы.
+
+        :return: Заголовок текущей страницы.
+        """
+        return self.page.title()
+
+    def inner_text(self, selector: str):
+        """
+        Возвращает текст элемента, выбранного селектором.
+
+        :param selector: Селектор элемента, текст которого нужно получить.
+        :return: Текст выбранного элемента.
+        """
+        return self.page.inner_text(selector)
+
     def query_selector_all(self, selector):
         """
         Возвращает все элементы на странице, которые соответствуют
@@ -42,10 +76,4 @@ class BasePage:
         """
         self.page.wait_for_selector(selector)
 
-    def click_button(self, selector: str):
-        """
-        Кликает по кнопке, соответствующей указанному селектору.
 
-        :param selector: Селектор кнопки, по которой нужно кликнуть.
-        """
-        self.page.click(selector)
