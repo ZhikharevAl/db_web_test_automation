e6692e6f5d6fd64853ccc8443f6ec5dffd14b26b
AlexeyZh <waltafunk@gmail.com>
2023-11-19 17:44:58 +0300

    feat: add product_page

---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    """
+    Класс ProductPage наследуется от BasePage и представляет
+            собой страницу продукта.
+    """
+
+    def click_on_the_product(self):
+        """
+        Метод для клика по продукту на странице.
+        """
+        self.click_button(ProductPageLocators.PRODUCTNAME)
+
+    def get_product_name(self):
+        """
+        Метод для получения имени продукта на странице.
+        """
+        return self.inner_text(ProductPageLocators.NAMEPRODUCT)
