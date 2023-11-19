bf5008db4828ca4cb5ddff4dc89d9d8b151b3a7f
AlexeyZh <waltafunk@gmail.com>
2023-11-19 14:42:59 +0300

    refactor: logging

--- tests/test_register_and_login.py
+++ tests/test_register_and_login.py
@@ -15,10 +15,19 @@ def test_register_and_login_functionality(page, caplog):
 
     with allure.step("Открываем страницу регистрации"):
         register_and_login_page.go_to()
+        logging.info("Страница регистрации открыта.")
     with allure.step("Регистрируем нового пользователя"):
         username = 'username'
         password = 'password'
         register_and_login_page.register_and_login(username, password)
-    with (allure.step("Проверяем правильность регистрации")):
-        assert register_and_login_page.is_logged_in(username) is True, \
+        logging.info(f"Зарегистрирован новый пользователь: {username}")
+    with allure.step("Проверяем правильность регистрации"):
+        is_logged_in = register_and_login_page.is_logged_in(username)
+        assert is_logged_in is True, \
             f"Неправильный результат для {username} и {password}"
+        if is_logged_in:
+            logging.info(f"Пользователь {username} успешно "
+                         f"зарегистрирован и вошел в систему.")
+        else:
+            logging.error(f"Регистрация или вход в систему "
+                          f"не удался для пользователя {username}.")
