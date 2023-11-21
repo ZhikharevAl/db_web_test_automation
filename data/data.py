0b0ced8823c58c146017c06edb9a7e34f3e1278f
AlexeyZh <waltafunk@gmail.com>
2023-11-21 03:01:20 +0300

    feat: add generator person data

---
+++ data/data.py
@@ -0,0 +1,12 @@
+from faker import Faker
+
+fake = Faker()
+
+
+class PersonData:
+    name: str = None
+    country: str = None
+    city: str = None
+    credit_card: int = None
+    month: int = None
+    year: int = None
---
+++ generator/generator_person_data.py
@@ -0,0 +1,12 @@
+from data.data import PersonData, fake
+
+
+def generate_person_data():
+    person = PersonData()
+    person.name = fake.name()
+    person.country = fake.country()
+    person.city = fake.city()
+    person.credit_card = fake.credit_card_number()
+    person.month = fake.month()
+    person.year = fake.year()
+    return person
