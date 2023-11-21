cb8100618c3a5dfb4a2943d7d502e32a518a2f17
AlexeyZh <waltafunk@gmail.com>
2023-11-22 01:44:52 +0300

    test: add test place order

---
+++ tests/test_place_order.py
@@ -0,0 +1,53 @@
+import logging
+
+import allure
+import pytest
+from allure_commons.types import Severity
+
+from generator.generator_person_data import generate_person_data
+from pages.cart_page import CartPage
+from pages.place_order_page import PlaceOrderPage
+
+
+@pytest.mark.smoke
+@allure.severity(Severity.CRITICAL)
+def test_place_order(page, user_account, add_to_cart, caplog):
+    """
+    Тестирование функциональности оформления заказа.
+    :param page: Экземпляр страницы для тестирования.
+    :param user_account: Фикстура для регистрации и входа в систему.
+    :param add_to_cart: Фикстура для добавления карточки в корзину.
+    :param caplog: Журнал для записи результатов теста.
+    """
+    caplog.set_level(logging.INFO)
+    person = generate_person_data()
+    place_order_page = PlaceOrderPage(page)
+    cart_page = CartPage(page)
+
+    with allure.step("Добавляем товар в корзину"):
+        cart_page.click_add_to_cart()
+        cart_page.click_cart_button()
+        logging.info("Товар добавлен в корзину.")
+
+    with allure.step("Открываем страницу оформления заказа"):
+        place_order_page.click_place_order()
+        logging.info("Страница оформления заказа открыта.")
+
+    with allure.step("Заполняем форму оформления заказа"):
+        place_order_page.fill_name(person.name)
+        place_order_page.fill_country(person.country)
+        place_order_page.fill_city(person.city)
+        place_order_page.fill_credit_card(person.credit_card)
+        place_order_page.fill_month(person.month)
+        place_order_page.fill_year(person.year)
+        place_order_page.place_order_screenshot()
+        logging.info("Форма оформления заказа заполнена.")
+
+    with allure.step("Оформляем заказ"):
+        place_order_page.place_order()
+        logging.info("Заказ оформлен.")
+
+    with (allure.step("Проверяем, что заказ успешно оформлен")):
+        assert place_order_page.message_thank_you() == \
+                "Thank you for your purchase!"
+        logging.info("Заказ успешно оформлен.")
