d8817c4a2f67c87562abf8251e9605618a5b23d6
AlexeyZh <waltafunk@gmail.com>
2023-11-22 01:28:38 +0300

    feat: update

--- generator/generator_person_data.py
+++ generator/generator_person_data.py
@@ -9,4 +9,5 @@ def generate_person_data():
     person.credit_card = fake.credit_card_number()
     person.month = fake.month()
     person.year = fake.year()
+    person.password = fake.password()
     return person
