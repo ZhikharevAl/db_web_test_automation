8828587d4ae8676dacd978154dd681cdc6d462d9
AlexeyZh <waltafunk@gmail.com>
2024-05-13 19:49:48 +0300

    refactor: Consolidate fixtures into TestFixtures class in conftest

--- conftest.py
+++ conftest.py
@@ -2,22 +2,23 @@ import pytest
 from playwright.sync_api import sync_playwright, BrowserContext, Page
 
 from generator.generator_person_data import generate_person_data
+from pages.base_pages.base_test import BaseTest
 from pages.product_page import ProductPage
 from pages.register_and_login_page import RegisterAndLoginPage
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(autouse=True)
 def browser() -> sync_playwright:
     """
     Фикстура для запуска и закрытия браузера.
     """
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)  # Запуск браузера
+        browser = p.chromium.launch(headless=True)
         yield browser
-        browser.close()  # Закрытие браузера после теста
+        browser.close()
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def page(browser: BrowserContext) -> Page:
     """
     Фикстура для открытия и закрытия новой страницы в браузере.
@@ -29,39 +30,36 @@ def page(browser: BrowserContext) -> Page:
     page.context.close()
 
 
-@pytest.fixture
-def user_account(page: Page, base_url: str) -> RegisterAndLoginPage:
-    """
-    Фикстура для регистрации и входа в систему.
-    """
-    register_and_login = RegisterAndLoginPage(page, base_url)
-    person = generate_person_data()
-    register_and_login.go_to(base_url)
-    username = person.name
-    password = person.password
-    register_and_login.register_and_login(username, password)
-    return register_and_login
+class TestFixtures(BaseTest):
+    @pytest.fixture
+    def user_account(self, page: Page, base_url: str) -> RegisterAndLoginPage:
+        """
+        Фикстура для регистрации и входа в систему.
+        """
 
-
-@pytest.fixture
-def add_to_cart(page: Page, base_url: str) -> ProductPage:
-    """
-    Фикстура для добавления карточки в корзину.
-    """
-    product_page = ProductPage(page, base_url)
-    product_page.click_on_the_product()
-    return product_page
-
-
-@pytest.fixture(params=[True, False])
-def user_account_authorized(request, page, base_url):
-    if request.param:
-        register_and_login = RegisterAndLoginPage(page, base_url)
         person = generate_person_data()
-        register_and_login.go_to(base_url)
+        self.register_and_login_page.go_to(base_url)
         username = person.name
         password = person.password
-        register_and_login.register_and_login(username, password)
-        return register_and_login
-    else:
-        return None
+        self.register_and_login_page.register_and_login(username, password)
+        return self.register_and_login_page
+
+    @pytest.fixture
+    def add_to_cart(self, page: Page) -> ProductPage:
+        """
+        Фикстура для добавления карточки в корзину.
+        """
+        self.product_page.click_on_the_product()
+        return self.product_page
+
+    @pytest.fixture(params=[True, False])
+    def user_account_authorized(self, request, page, base_url):
+        if request.param:
+            person = generate_person_data()
+            self.register_and_login_page.go_to(base_url)
+            username = person.name
+            password = person.password
+            self.register_and_login_page.register_and_login(username, password)
+            return self.register_and_login_page
+        else:
+            return None
