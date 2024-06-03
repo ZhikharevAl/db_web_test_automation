6e4185dd28fbd2f55fb12fcaceeb3137d33495c0
AlexeyZh <waltafunk@gmail.com>
2024-06-03 05:04:29 +0300

    style: black

--- data/data.py
+++ data/data.py
@@ -9,9 +9,7 @@ class PersonData:
     name: str = field(default_factory=lambda: fake.name())
     country: str = field(default_factory=lambda: fake.country())
     city: str = field(default_factory=lambda: fake.city())
-    credit_card: str = field(
-        default_factory=lambda: fake.credit_card_number("visa")
-    )
+    credit_card: str = field(default_factory=lambda: fake.credit_card_number("visa"))
     month: str = field(default_factory=lambda: fake.month())
     year: str = field(default_factory=lambda: fake.year())
     password: str = field(default_factory=lambda: fake.password())
